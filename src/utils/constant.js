2986fcdfffc1977c59cb398693ad52566711c180
Yadav, Kundan Kumar <KundanKumar.Yadav@Clarivate.com>
2023-04-12 23:06:45 +0530

    env file setup

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,5 +1,4 @@
-const YOUTUBE_API_KEY = "";
+const YOUTUBE_API_KEY = process.env.REACT_APP_API_KEY;
 
 export const YOUTUBE_VIDEOS_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
-  YOUTUBE_API_KEY;
+  `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${YOUTUBE_API_KEY}`;
